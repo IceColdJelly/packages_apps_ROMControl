193058c5753f4ea702f976a568ac653ac4a00d41
LorDClockaN <davor@losinj.com>
2012-10-25 20:53:48 +0200

    Add setting to allow lockscreen theming - AOKP
    PS2: grammar nazi
    
    Change-Id: I119fb4288d0033df7b5e0c877655d09ed99fee7e
    
    Conflicts:
    
    	res/values/strings.xml
    	src/com/aokp/romcontrol/fragments/Lockscreens.java

--- src/com/aokp/romcontrol/fragments/Lockscreens.java
+++ src/com/aokp/romcontrol/fragments/Lockscreens.java
@@ -85,7 +85,7 @@ public class Lockscreens extends AOKPPreferenceFragment implements
     private static final String PREF_LOCKSCREEN_AUTO_ROTATE = "lockscreen_auto_rotate";
     private static final String PREF_STOCK_MUSIC_LAYOUT = "lockscreen_stock_music_layout";
     private static final String PREF_LOCKSCREEN_VIBRATE = "lockscreen_vibrate";
-
+    private static final String PREF_USE_LOCKSCREEN_THEMES = "use_lockscreen_themes";
 
     public static final int REQUEST_PICK_WALLPAPER = 199;
     public static final int REQUEST_PICK_CUSTOM_ICON = 200;
@@ -114,6 +114,7 @@ public class Lockscreens extends AOKPPreferenceFragment implements
     CheckBoxPreference mLockscreenAutoRotate;
     CheckBoxPreference mStockMusicLayout;
     CheckBoxPreference mLockscreenVibrate;
+    CheckBoxPreference mUseLSThemes;
 
     ListPreference mTargetNumber;
 
@@ -203,6 +204,10 @@ public class Lockscreens extends AOKPPreferenceFragment implements
         mLockscreenVibrate = (CheckBoxPreference) findPreference(PREF_LOCKSCREEN_VIBRATE);
         mLockscreenVibrate.setChecked(Settings.System.getInt(getActivity().getApplicationContext().getContentResolver(),
                 Settings.System.LOCKSCREEN_VIBRATE_DISABLED, 1) == 1);
+
+        mUseLSThemes = (CheckBoxPreference)findPreference(PREF_USE_LOCKSCREEN_THEMES);
+        mUseLSThemes.setChecked(Settings.System.getBoolean(mContext
+                .getContentResolver(), Settings.System.USE_LOCKSCREEN_THEMES, false));
 
         mLockscreenWallpaper = findPreference("wallpaper");
 
@@ -383,6 +388,11 @@ public class Lockscreens extends AOKPPreferenceFragment implements
             Log.e("RC_Lockscreens", "key: " + preference.getKey());
             return Settings.System.putInt(getActivity().getContentResolver(), preference.getKey(),
                     ((CheckBoxPreference)preference).isChecked() ? 1 : 0);
+        } else if (preference == mUseLSThemes) {
+            Settings.System.putBoolean(mContext.getContentResolver(),
+                    Settings.System.USE_LOCKSCREEN_THEMES,
+                    ((CheckBoxPreference) preference).isChecked());
+            return true;
         }
         
         return super.onPreferenceTreeClick(preferenceScreen, preference);
